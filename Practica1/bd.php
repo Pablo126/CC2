a90705093da0e736d2b1535b3ea2130acd31b3e6
root <root@localhost.localdomain>
2017-03-27 12:31:35 -0400

    Conexión establecida

--- Practica1/bd.php
+++ Practica1/bd.php
@@ -1,57 +1,32 @@
 <?php
-    class MySQL
-    {
-        private $servername; 
-        private $username = 'pablo';
-        private $password = "Pablo@1234";
-        private $database = "practica1cc";
-        private $dbport = 80;
-        private $conexion; private $total_consultas;
-        public function MySQL()
-        { 
-            if(!isset($this->conexion))
-            {
-              $this->conexion = ('192.168.10.83',$username,$password))
-                or die(mysql_error());
-              mysql_select_db("practica1cc",$this->conexion) or die(mysql_error());
-            }
-        }
-        public function consulta($consulta)
-        { 
-            $this->total_consultas++; 
-            $resultado = mysql_query($consulta,$this->conexion);
-            if(!$resultado){ 
-              echo 'MySQL Error: ' . mysql_error();
-              exit;
-            }
-        return $resultado;
-        }
-        public function fetch_array($consulta)
-        {
-        return mysql_fetch_array($consulta);
-        }
-        public function num_rows($consulta)
-        {
-        return mysql_num_rows($consulta);
-        }
-        public function getTotalConsultas()
-        {
-        return $this->total_consultas; 
-        }
-        
-        
-        public function consultaArray($sql)
-        {
-            $rows = [];
-            $consulta = $this->consulta($sql);
-            if($this->num_rows($consulta)>0){
-                while($resultados = $this->fetch_array($consulta))
-                { 
-                    $rows[] = $resultados{0};  
-                }
-            }
-        return $rows;
-        }
-    }
-    
-    ?>
+
+class MySQLDB
+{
+   	private $connection;          // The MySQL database connection
+
+   	/* Class constructor */
+   	function MySQLDB(){
+      		/* Make connection to database */
+      		$this->connection = mysql_connect('192.168.10.83:3306', 'pablo', 'Pablo@1234') or die(mysql_error());
+		mysql_select_db('practica1cc', $this->connection) or die(mysql_error());
+	}
+	function consulta($consulta)
+	{
+		$resultado = mysql_query($consulta,$this->connection);
+		if(!resultado)
+		{
+			echo "MySQL Error: ".mysql_error();
+			exit;
+		}
+		return $resultado;
+	}
+	function fetch_array($consulta)
+	{
+		return mysql_fetch_array($consulta);
+	}
+	function num_rows($consulta)
+	{
+		return mysql_num_rows($consulta);
+	}
+}
+?>
